9243ba1a905137fa04d911ca628068dc84779401
Gideon Feldberg <gideonfeldberg@Gideons-MacBook-Pro.local>
2022-12-03 12:10:41 -0500

    day 1 finished

--- 1/1.py
+++ 1/1.py
@@ -4,17 +4,21 @@ total=0
 with open("./input.txt") as file:
     for line in file:
         stripped_line = line.rstrip()
+        print(stripped_line)
         if not stripped_line:
             idx += 1
             elf_totals.append(total)
             total=0
             continue
         total+=int(stripped_line)
-
+    # Need to include final set of totals
+    elf_totals.append(total)
 
 max = 0
 for index, value in enumerate(elf_totals):
     if value > max:
         max = value
 
-print(f'MAX={max}')
+print(f'MAX_PART1={max}')
+elf_totals.sort(reverse=True)
+print(f'MAX PART2={sum(elf_totals[:3])}')
