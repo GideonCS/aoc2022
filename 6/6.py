40f08539d772c25604ba9a995c64cd0f35abf0b8
Gideon Feldberg <gideonfeldberg@Gideons-MacBook-Pro.local>
2022-12-07 10:57:24 -0500

    day 6 slight update

--- 6/6.py
+++ 6/6.py
@@ -1,9 +1,8 @@
 def get_marker():
-    buff = []
     LENGTH_OF_MESSAGE = 14
     with open("./input.txt") as file:
         for line in file:
-            for idx, char in enumerate(line):
+            for idx, _ in enumerate(line):
                 next_four = line[idx : idx + LENGTH_OF_MESSAGE]
                 next_four_set = set(next_four)
                 if len(next_four_set) == LENGTH_OF_MESSAGE:
